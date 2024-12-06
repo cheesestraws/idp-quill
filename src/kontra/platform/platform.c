4fa62ec8257aee0053501c8e711cc760b5c6614c
cheesestraws <rob@midworld.co.uk>
2024-12-06 21:50:18 +0000

    hack the hell out of platform/platform.c to build on posix

--- src/kontra/platform/platform.c
+++ src/kontra/platform/platform.c
@@ -9,56 +9,37 @@
  * 10.11.2022   tstih
  *
  */
+#include <unistd.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-/* Call bdos function 10 C_READSTR! */
-extern unsigned char bdos(unsigned char fn, unsigned int param);
-#define C_READSTR   10
-#define C_WRITE     2
-
 void _printstring(const char *s) {
-    /* first write string*/
-    while(*s) { 
-        if (*s=='\n') bdos(C_WRITE,'\r');
-        bdos(C_WRITE,*s++); 
-    }
+	printf("%s", s);
 }
 
 char* platform_readline(char *out, uint8_t maxchars) {
-    /* how many characters were read */
-    uint8_t numchars;
-
-    /* allocate required buffer. */
-    char *mem=malloc(maxchars + 2);
-    mem[0]=maxchars; /* limit number of chars */
-    bdos(C_READSTR,(unsigned int)mem); /* read chars from console... */
-
-    /* extract length */
-    numchars=mem[1];
-
-    /* copy buffer to output and zero terminate it! */
-    if (numchars>0) 
-        memcpy(out, &(mem[2]), numchars);
-    out[numchars]=0;
-
-    /* free the buffer */
-    free(mem);
-
-    /* add newline */
-    _printstring("\n");    
-
-    /* and return string */
-    return out;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read = 0;
+	
+	read = getline(&line, &len, stdin);
+	if (read == -1) {
+		free(line);
+		return NULL;
+	}
+	
+	strncpy(out, line, maxchars);
+	free(line);
+	_printstring("\n");
+	return out;
 }
 
-void platform_clear(uint8_t area) { area; } /* no clear! */
+void platform_clear(uint8_t area) { } /* no clear! */
 
 void platform_write(uint8_t area, const char *s, bool newline) {
-    area;
     _printstring(s);
     if (newline) _printstring("\n");
 }
@@ -68,48 +49,11 @@ int platform_rndgen() {
 }
 
 /* we'll pretend its already pressed */
-int platform_kbhit() { return kbhit();} 
-
-
-/* mem top is c000 (start of shared memory for idp) */
-size_t _memtop() {
-    return (size_t )0xc000;
-}
-
-/* Non standard function to sleep (in milliseconds) */
-void _msleep(int millisec) {
-    millisec;
-    /* For 1 millisec we need to sleep 
-       0.001/(1/4000000)=4000 t-states,
-       but  
-        ...outer loop adds 38 t-states 
-        ...push and pop commands add 42 t-states to everything 
-       so we need 3962 t-states for 1ms + outer loop overhead */
-    __asm
-        pop     de                      ; return address
-        pop     hl                      ; milliseconds
-        ;; restore stack
-        push    hl 
-        push    de
-        ;; now count hl
-    msl_loop:
-        ld      b,#233                  ; 7 t-states outer counter 
-        ;; inner loop is 17 * 233 t-states = 3961 t-states
-    msl_inner_loop:
-        nop                             ; 4 t-states
-        djnz    msl_inner_loop          ; 13/8 t-states
-        ;; and loop.
-        ;; this loop and ld b adds 38 t-states to each millisecond
-        dec     hl                      ; 6 t-states
-        ld      a,h                     ; 9 t-states
-        or      l                       ; 4 t-states
-        jr      nz,msl_loop             ; 12/7 t-states
-    __endasm;
-}
-
-
+int platform_kbhit() { 
+	return getchar();
+} 
 
 /* nothing, for now... */
 void platform_pause(int s50) {
-    _msleep(20*s50);
+    usleep(20000*s50);
  }
